3b363ff4c71e9cb1287b12b1220309b200b33958
Robert Bierly <robert.bierly4@gmail.com>
2021-05-04 14:51:17 -0400

    init cassandra

--- src/main/java/com/revature/group2/controllers/UserController.java
+++ src/main/java/com/revature/group2/controllers/UserController.java
@@ -1,5 +1,19 @@
 package com.revature.group2.controllers;
 
-public class UserController {
+import org.reactivestreams.Publisher;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.group2.beans.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserController {
+	
+	@PostMapping(value="{userId}/cards")
+	public Publisher<User> collectCard() {
+		return null;
+	}
+	
 }
