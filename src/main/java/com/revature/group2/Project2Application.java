7fb1877ae1b4ad8a9c856200cfae7f73cf438398
Robert Bierly <robert.bierly4@gmail.com>
2021-05-03 22:28:09 -0400

    file setup

--- src/main/java/com/revature/group2/Project2Application.java
+++ src/main/java/com/revature/group2/Project2Application.java
@@ -1,4 +1,4 @@
-package com.revature.group2.project2;
+package com.revature.group2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/revature/group2/project2/beans/User.java
+++
@@ -1,5 +0,0 @@
-package com.revature.group2.project2.beans;
-
-public class User {
-
-}
