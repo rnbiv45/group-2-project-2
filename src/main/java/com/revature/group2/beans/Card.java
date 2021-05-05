4c67d0d09f62adc02ed36a3727cedb0f3c1ed8eb
Robert Bierly <robert.bierly4@gmail.com>
2021-05-05 14:34:47 -0400

    added lombok

--- src/main/java/com/revature/group2/beans/Card.java
+++ src/main/java/com/revature/group2/beans/Card.java
@@ -5,14 +5,19 @@ import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 import org.springframework.data.cassandra.core.mapping.Table;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 @Table(value=Card.tableName)
+@Data
 public class Card {
 	
 	@org.springframework.data.annotation.Transient
 	public static final String tableName = "card";
 	
 	@PrimaryKey
-	private CardPrimaryKey cardPrimaryKey;
+	@Getter @Setter private CardPrimaryKey cardPrimaryKey;
 	@Column
 	private String name;
 	@Column
@@ -23,101 +28,4 @@ public class Card {
 	private Integer defenseValue;
 	@Column
 	private Integer damageValue;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public CardPrimaryKey getCardPrimaryKey() {
-		return cardPrimaryKey;
-	}
-	public void setCardPrimaryKey(CardPrimaryKey cardPrimaryKey) {
-		this.cardPrimaryKey = cardPrimaryKey;
-	}
-	public boolean isUnique() {
-		return isUnique;
-	}
-	public void setUnique(boolean isUnique) {
-		this.isUnique = isUnique;
-	}
-	public Integer getAttackValue() {
-		return attackValue;
-	}
-	public void setAttackValue(Integer attackValue) {
-		this.attackValue = attackValue;
-	}
-	public Integer getDefenseValue() {
-		return defenseValue;
-	}
-	public void setDefenseValue(Integer defenseValue) {
-		this.defenseValue = defenseValue;
-	}
-	public Integer getDamageValue() {
-		return damageValue;
-	}
-	public void setDamageValue(Integer damageValue) {
-		this.damageValue = damageValue;
-	}
-	public static String getTablename() {
-		return tableName;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((attackValue == null) ? 0 : attackValue.hashCode());
-		result = prime * result + ((cardPrimaryKey == null) ? 0 : cardPrimaryKey.hashCode());
-		result = prime * result + ((damageValue == null) ? 0 : damageValue.hashCode());
-		result = prime * result + ((defenseValue == null) ? 0 : defenseValue.hashCode());
-		result = prime * result + (isUnique ? 1231 : 1237);
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Card other = (Card) obj;
-		if (attackValue == null) {
-			if (other.attackValue != null)
-				return false;
-		} else if (!attackValue.equals(other.attackValue))
-			return false;
-		if (cardPrimaryKey == null) {
-			if (other.cardPrimaryKey != null)
-				return false;
-		} else if (!cardPrimaryKey.equals(other.cardPrimaryKey))
-			return false;
-		if (damageValue == null) {
-			if (other.damageValue != null)
-				return false;
-		} else if (!damageValue.equals(other.damageValue))
-			return false;
-		if (defenseValue == null) {
-			if (other.defenseValue != null)
-				return false;
-		} else if (!defenseValue.equals(other.defenseValue))
-			return false;
-		if (isUnique != other.isUnique)
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
-	}
-	@Override
-	public String toString() {
-		return "Card [name=" + name + ", cardPrimaryKey=" + cardPrimaryKey + ", isUnique=" + isUnique + ", attackValue="
-				+ attackValue + ", defenseValue=" + defenseValue + ", damageValue=" + damageValue + "]";
-	}
-	
-	
-
 }
--- src/main/java/com/revature/group2/beans/CardPrimaryKey.java
+++ src/main/java/com/revature/group2/beans/CardPrimaryKey.java
@@ -12,7 +12,10 @@ import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
 
+import lombok.Data;
+
 @PrimaryKeyClass
+@Data
 public class CardPrimaryKey implements Serializable{
 	/**
 	 * 
@@ -55,89 +58,4 @@ public class CardPrimaryKey implements Serializable{
 			ordering = Ordering.DESCENDING)
 	@CassandraType(type = Name.BOOLEAN)
 	boolean isBanned;
-	public Type getType() {
-		return type;
-	}
-	public void setType(Type type) {
-		this.type = type;
-	}
-	public Archetype getArchetype() {
-		return archetype;
-	}
-	public void setArchetype(Archetype archetype) {
-		this.archetype = archetype;
-	}
-	public UUID getId() {
-		return id;
-	}
-	public void setId(UUID id) {
-		this.id = id;
-	}
-	public int getRarity() {
-		return rarity;
-	}
-	public void setRarity(int rarity) {
-		this.rarity = rarity;
-	}
-	public Set<Effect> getEffects() {
-		return effects;
-	}
-	public void setEffects(Set<Effect> effects) {
-		this.effects = effects;
-	}
-	public boolean isBanned() {
-		return isBanned;
-	}
-	public void setBanned(boolean isBanned) {
-		this.isBanned = isBanned;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((archetype == null) ? 0 : archetype.hashCode());
-		result = prime * result + ((effects == null) ? 0 : effects.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + (isBanned ? 1231 : 1237);
-		result = prime * result + rarity;
-		result = prime * result + ((type == null) ? 0 : type.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		CardPrimaryKey other = (CardPrimaryKey) obj;
-		if (archetype != other.archetype)
-			return false;
-		if (effects == null) {
-			if (other.effects != null)
-				return false;
-		} else if (!effects.equals(other.effects))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (isBanned != other.isBanned)
-			return false;
-		if (rarity != other.rarity)
-			return false;
-		if (type != other.type)
-			return false;
-		return true;
-	}
-	@Override
-	public String toString() {
-		return "CardPrimaryKey [type=" + type + ", archetype=" + archetype + ", id=" + id + ", rarity=" + rarity
-				+ ", effects=" + effects + ", isBanned=" + isBanned + "]";
-	}
-
-	
-	
 }
