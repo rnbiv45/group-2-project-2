538e016887f274cc0f60dd853010cee4e1762a41
ngelezinis <ngelezinis@gmail.com>
2021-05-05 09:56:30 -0500

    changed primary key column to primary key solving the problem preventing compiling

--- src/main/java/com/revature/group2/services/CardServiceImp.java
+++ src/main/java/com/revature/group2/services/CardServiceImp.java
@@ -2,6 +2,8 @@ package com.revature.group2.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.revature.group2.beans.Card;
 import com.revature.group2.beans.User;
 import com.revature.group2.repos.CardRepo;
@@ -11,13 +13,15 @@ import reactor.core.publisher.Flux;
 public class CardServiceImp implements CardService {
 	private CardRepo cardRepo;
 	
+	@Autowired
 	public void setCardRepo(CardRepo cardRepo) {
 		this.cardRepo = cardRepo;
 	}
 
 	@Override
 	public Flux<Card> getCardsFromSystem() {
-		return cardRepo.findAll();
+		return null;
+		//return cardRepo.findAll();
 	}
 
 	@Override
@@ -40,64 +44,22 @@ public class CardServiceImp implements CardService {
 
 	@Override
 	public void addCardToSystem(Card card) {
-		cardRepo.insert(card);
+		//cardRepo.insert(card);
 		return;
 		
 	}
 
 	@Override
 	public void removeCardFromSystem(Card card) {
-		cardRepo.delete(card);
+		//cardRepo.delete(card);
 		return;
 		
 	}
 
 	@Override
 	public void setCard(Card card) {
-		cardRepo.save(card);
+		//cardRepo.save(card);
 		return;
-		
-	}
-
-	@Override
-	public List<Card> getCardsFromSystem() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Card> getCardsFromUser(User user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Card> getCardsMissingFromSystem(User user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Card> getMetaCards() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void addCardToSystem(Card card) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void removeCardFromSystem(Card card) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setCard(Card card) {
-		// TODO Auto-generated method stub
 		
 	}
 
