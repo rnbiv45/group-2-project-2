b49d320ed13a70854f28af7f8417f834524c354e
NGelezinis <ngelez2@uic.edu>
2021-05-06 11:09:52 -0500

    added spring annotations to the user and deck beans

--- src/main/java/com/revature/group2/beans/Deck.java
+++ src/main/java/com/revature/group2/beans/Deck.java
@@ -1,14 +1,42 @@
 package com.revature.group2.beans;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.CassandraType.Name;
+import org.springframework.data.cassandra.core.mapping.Column;
 
 import com.revature.group2.beans.Archetype;;
 
 
 public class Deck {
+	@PrimaryKeyColumn(
+			name="primaryArchetype",
+			ordinal=0,
+			type = PrimaryKeyType.PARTITIONED,
+			ordering = Ordering.DESCENDING)
+	@CassandraType(type = Name.TEXT)
 	private Archetype primaryArchetype;
+	@PrimaryKeyColumn(
+			name="secondaryArchetype",
+			ordinal=1,
+			type = PrimaryKeyType.PARTITIONED,
+			ordering = Ordering.DESCENDING)
+	@CassandraType(type = Name.TEXT)
 	private Archetype secondaryArchetype;
+	@PrimaryKeyColumn(
+			name="uuid",
+			ordinal=2,
+			type = PrimaryKeyType.CLUSTERED)
+	@CassandraType(type = Name.UUID)
+	private UUID uuid;
+	@Column
 	private User creator;
+	@Column
 	private Map<Card, Integer> cards;
 	
 	
