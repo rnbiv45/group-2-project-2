a75d10cd8fb4fc8ecb44fa80a6d8449353bd4135
NGelezinis <ngelez2@uic.edu>
2021-05-06 16:30:11 -0500

    setup Tests

---
+++ src/test/java/com/revature/group2/controllers/UserControllerTest.java
@@ -0,0 +1,36 @@
+package com.revature.group2.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.revature.group2.services.UserService;
+
+@ExtendWith(SpringExtension.class)
+public class UserControllerTest {
+	@TestConfiguration
+	static class Configuration {
+		@Bean
+		public UserController getUserController(UserService userService) {
+			UserController userController = new UserController();
+			userController.setUserService(userService);
+			return userController;
+			
+		}
+		@Bean
+		public UserService getUserService(UserService userService) {
+			return Mockito.mock(UserService.class);
+			
+		}
+	}
+	
+	@Autowired
+	private UserController userController;
+	
+	@Autowired
+	private UserService userService;
+
+}
