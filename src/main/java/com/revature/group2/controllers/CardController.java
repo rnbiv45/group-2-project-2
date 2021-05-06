de19200cb15380b8e69951bbfebc4b00e3bf3c3e
ngelezinis <ngelezinis@gmail.com>
2021-05-05 23:31:22 -0500

    added tests

--- src/main/java/com/revature/group2/controllers/CardController.java
+++ src/main/java/com/revature/group2/controllers/CardController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,6 +51,11 @@ public class CardController {
 	public Flux<Card> getAllCards(){
 		return cardService.getCardsFromSystem();
 		
+	}
+	
+	//add a card
+	public void addCard(@RequestBody Card card) {
+		cardService.addCardToSystem(card);
 	}
 
 }
