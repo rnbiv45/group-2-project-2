67b4d960dbc0066b980426513363447ec1d06f16
NGelezinis <ngelez2@uic.edu>
2021-05-14 14:28:46 -0500

    ctrl + shift + o for a bunch of files

--- src/main/java/com/revature/group2/controllers/DeckController.java
+++ src/main/java/com/revature/group2/controllers/DeckController.java
@@ -6,18 +6,9 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
-import org.springframework.web.server.ServerWebExchange;
-
-import com.revature.group2.beans.Card;
-import com.revature.group2.beans.Deck;
-import com.revature.group2.beans.User;
-import com.revature.group2.services.DeckService;
-import com.revature.group2.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -28,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 
 import com.revature.group2.beans.Archetype;
+import com.revature.group2.beans.Card;
 import com.revature.group2.beans.Deck;
 import com.revature.group2.beans.User;
 import com.revature.group2.services.DeckService;
